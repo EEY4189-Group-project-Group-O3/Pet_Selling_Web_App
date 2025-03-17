560e088c1d35f0d52a7cdb77bc2ea217ab5cc77e
yusufyuhan <115976807+mlyusufyuhan@users.noreply.github.com>
2025-03-17 22:03:03 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for core project.
+core app url here
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
